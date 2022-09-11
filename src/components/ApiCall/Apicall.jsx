afc7b93f6b334fe08852737b39017ef54fd69281
Manuela Gutierrez <manuelagutierrezgutierrez@gmail.com>
2022-09-11 09:48:02 +0200

    paint characters cards

--- src/components/ApiCall/Apicall.jsx
+++ src/components/ApiCall/Apicall.jsx
@@ -1,13 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const ApiCall = () => {
+	const [characters, setCharacters] = useState([]);
+	// let { info, results } = characters;
+
 	useEffect(() => {
 		fetch("https://rickandmortyapi.com/api/character")
 			.then((res) => res.json())
-			.then((res) => console.log(res));
+			.then((res) => setCharacters(res.results));
 	}, []);
 
-	return <div></div>;
+	return (
+		<div>
+			{characters.map((character) => (
+				<div>
+					<p>{character.name}</p>
+					<img src={character.image} alt="Character"></img>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default ApiCall;
