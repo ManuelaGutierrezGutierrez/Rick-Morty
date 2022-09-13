468bc930bd42c7bfc5f3813a501b49e2aecd0301
Manuela Gutierrez <manuelagutierrezgutierrez@gmail.com>
2022-09-13 21:14:24 +0200

    update btn styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,10 @@ import "./Header.scss";
 
 const Header = () => {
   return (
-    <div className='header'><img src="../assets/logo.webp" className='logo' alt="logo"></img></div>
+    <div className='header'>
+    <img src='../assets/rick-morty-round-4.webp' alt='logo round' className='roudLogo'></img>
+    <img src="../assets/logo.webp" className='logo' alt="logo"></img>
+    </div>
   )
 }
 
